b9cd889a55da3cf75b69ad91226c098b9cd593ed
Chanh03 <ngovietanh121@gmail.com>
2025-03-03 14:46:29 +0700

    basic, event, binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderLayoutComponent } from './component/header-layout/header-layout.component';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderLayoutComponent, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'basic-angular-project';
+  // Text
+  title = {
+    name: 'VANH DZ',
+    password: 'vietanh123',
+    address: {
+      district: '12',
+      city: 'Ho Chi Minh',
+    },
+  };
+  // Properties
+  isDisable = true;
+
+  //Attributes
+  dataImg = {
+    scr: 'https://placehold.co/600x400',
+    alt: 'img.jpg',
+  };
+  isHidden = true;
+  isHiddenMessage = 'Show';
+  show() {
+    this.isHidden = !this.isHidden;
+    if (this.isHidden) {
+      this.isHiddenMessage = 'Show';
+    } else {
+      this.isHiddenMessage = 'Hide';
+    }
+    console.log(this.isHidden);
+  }
+  clickCount = 0;
+  counter() {
+    this.clickCount++;
+  }
+  //
+  bindingMessage = '';
 }
---
+++ src/app/component/header-layout/header-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-layout',
+  imports: [],
+  standalone: true,
+  templateUrl: './header-layout.component.html',
+  styleUrl: './header-layout.component.css',
+})
+export class HeaderLayoutComponent {}
